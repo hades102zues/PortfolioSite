4465795a03af203c6ff2941890900bf06b99eb84
Joshua Wiggins <joshua_kar@hotmail.com>
2019-05-04 17:31:25 -0400

    fail

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -8,68 +8,13 @@ import { Parallax } from "react-parallax";
 import showcase_img from "./assests/paralax1.jpeg";
 import xd_img from "./assests/1200px-Adobe_XD_CC_icon.svg.png";
 
+import Header from "./Sections/Header/Header.js";
+
 class MainPage extends Component {
 	render() {
 		return (
 			<div className="mainPage">
-				<header>
-					<Navbar
-						collapseOnSelect
-						expand="lg"
-						bg="dark"
-						variant="dark"
-						className="nav_space"
-					>
-						<div className="container wrapper">
-							<Link
-								to="showcase"
-								activeClass="sec_pop"
-								smooth
-								duration={1000}
-							>
-								<Navbar.Brand>
-									<span class=" brand">Joshua Wiggins</span>
-								</Navbar.Brand>
-							</Link>
-							<Navbar.Toggle aria-controls="responsive-navbar-nav" />
-							<Navbar.Collapse id="responsive-navbar-nav">
-								<Nav className="mr-auto" />
-								<Nav className="adjust">
-									<Link to="services" duration={1000} smooth>
-										<Nav.Link href="#">
-											<span className="nav_text">
-												Serivces
-											</span>
-										</Nav.Link>
-									</Link>
-
-									<Link to="portfolio" duration={1000} smooth>
-										<Nav.Link href="#">
-											<span className="nav_text">
-												Portfolio
-											</span>
-										</Nav.Link>
-									</Link>
-									<Link to="about" duration={1000} smooth>
-										<Nav.Link href="#">
-											<span className="nav_text">
-												About
-											</span>
-										</Nav.Link>
-									</Link>
-
-									<Link to="contact" duration={1000} smooth>
-										<Nav.Link href="#">
-											<span className="nav_text">
-												Contact
-											</span>
-										</Nav.Link>
-									</Link>
-								</Nav>
-							</Navbar.Collapse>
-						</div>
-					</Navbar>
-				</header>
+				<Header />
 
 				<Element name="showcase">
 					<Parallax
--- src/pages/MainPage/Sections/Header/Header.js
+++ src/pages/MainPage/Sections/Header/Header.js
@@ -1,11 +1,61 @@
 import React from "react";
 import styles from "./Header.module.css";
 
+import { Card, Carousel, Navbar, Nav } from "react-bootstrap";
+import { Link, Element } from "react-scroll";
+
 const header = () => {
 	return (
-		<div className={styles.header}>
-			<p>I am header</p>
-		</div>
+		<header>
+			<Navbar
+				collapseOnSelect
+				expand="lg"
+				bg="dark"
+				variant="dark"
+				className="nav_space"
+			>
+				<div className="container wrapper">
+					<Link
+						to="showcase"
+						activeClass="sec_pop"
+						smooth
+						duration={1000}
+					>
+						<Navbar.Brand>
+							<span class=" brand">Joshua Wiggins</span>
+						</Navbar.Brand>
+					</Link>
+					<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+					<Navbar.Collapse id="responsive-navbar-nav">
+						<Nav className="mr-auto" />
+						<Nav className="adjust">
+							<Link to="services" duration={1000} smooth>
+								<Nav.Link href="#">
+									<span className="nav_text">Serivces</span>
+								</Nav.Link>
+							</Link>
+
+							<Link to="portfolio" duration={1000} smooth>
+								<Nav.Link href="#">
+									<span className="nav_text">Portfolio</span>
+								</Nav.Link>
+							</Link>
+							<Link to="about" duration={1000} smooth>
+								<Nav.Link href="#">
+									<span className="nav_text">About</span>
+								</Nav.Link>
+							</Link>
+
+							<Link to="contact" duration={1000} smooth>
+								<Nav.Link href="#">
+									<span className="nav_text">Contact</span>
+								</Nav.Link>
+							</Link>
+						</Nav>
+					</Navbar.Collapse>
+				</div>
+			</Navbar>
+		</header>
 	);
 };
 
